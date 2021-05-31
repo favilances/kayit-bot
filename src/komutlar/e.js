8e1b39af9e8032a07e63e3c3923cc56163c1038b
favilances <78090594+favilances@users.noreply.github.com>
2021-05-31 16:32:54 +0300

    Add files via upload

---
+++ src/komutlar/e.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+
+
+exports.run = async (client, message, args) => {
+    if(!message.member.roles.cache.has(client.kayıt.yetkili)) return message.channel.send(
+        new Discord.MessageEmbed()
+        .setColor(client.embed.renk)
+        .setAuthor(`${client.user.username} Kayıt Sistemi`, client.user.displayAvatarURL({dynamic: true}))
+        .setDescription(`Bu komutu kullanmak için <@&${yetkilick.id}> rolüne sahip olmalısın!`)
+        .setFooter(client.embed.footer)
+        )
+          if (message.channel.id !== client.kayıt.kanal) return message.channel.send(`Kayıt Kanalı <#${client.kayıt.kanal}> Suan Burda`);
+          let user = message.mentions.members.first();
+          if (!user) return message.channel.send(`Erkek Olarak Kayıt Edeceğin Kullanıcıyı Belirtmelisin ! `)
+          let guild = message.guild
+  let isim = args[1]
+  let yas = args[2]
+  if (!isim) return message.channel.send(`İsmini girmelisin.`)
+  if (!yas) return message.channel.send(`Yaşını girmelisin.`)
+  user.setNickname(`${client.kayıt.tag} ${isim} | ${yas}`)
+  user.roles.add(client.kayıt.erkek)
+  message.channel.send(`${user} **Sunucuya Kayıt Oldun İyi Eğlenceler**`)
+  let log = message.channel.guild.channels.cache.get(client.kayıt.log);
+ log.send(`${user} **Adlı Kullanıcı Başarılı Şekilde Kayıt Oldu** ✅ `);
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["erkek"],
+  permLevel: 0,
+  kategori: "Kayıt"
+}
+
+exports.help = {
+  name: 'e',
+  description: "Sunucuya Erkek Olarak kayıtolmaya yarar",
+  usage: 'e <isim> <yaş>'
+}
---
+++ src/komutlar/test.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+exports.run = async(client, message, args) => {
+
+ 
+};
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'test',
+    description: 'Test komutu',
+    usage: 'prefix+test'
+  };
